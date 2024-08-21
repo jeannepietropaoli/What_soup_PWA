85432f8b7bceba2f1c549d58352b98338336c24d
Jeanne Pietropaoli <j.pietropaoli@yahoo.fr>
2024-08-20 20:48:08 -0400

    formulaire de creation de ramen: script de gestion du form

---
+++ scripts/ramen-creation-form.js
@@ -0,0 +1,35 @@
+import RamenRecipe from './RamenRecipe.js';
+
+const form = document.getElementById('ramen-creation-form');
+
+form.addEventListener('submit', function(event) {
+    // event.preventDefault();
+    const bouillon = document.querySelector('input[name="bouillon"]:checked').value || null;
+    const nouilles = document.querySelector('input[name="nouilles"]:checked').value || null;
+    const proteine = document.querySelector('input[name="proteine"]:checked').value || null;
+    const legumes = Array.from(document.querySelectorAll('input[name="legumes"]:checked')).map(legume => legume.value);
+    const toppings = Array.from(document.querySelectorAll('input[name="toppings"]:checked')).map(topping => topping.value);
+    const epices = Array.from(document.querySelectorAll('input[name="epices"]:checked')).map(epice => epice.value);
+
+    const bouillonImg = document.querySelector('input[name="bouillon"]:checked').nextElementSibling.src;
+    const nouillesImg = document.querySelector('input[name="nouilles"]:checked').nextElementSibling.src;
+    const proteineImg = document.querySelector('input[name="proteine"]:checked').nextElementSibling.src;
+    const legumesImgs = Array.from(document.querySelectorAll('input[name="legumes"]:checked')).map(legume => legume.nextElementSibling.src);
+    const toppingsImgs = Array.from(document.querySelectorAll('input[name="toppings"]:checked')).map(topping => topping.nextElementSibling.src);
+    const epicesImgs = Array.from(document.querySelectorAll('input[name="epices"]:checked')).map(epice => epice.nextElementSibling.src);
+
+    const ingredientsImgs = [bouillonImg, nouillesImg, proteineImg, ...legumesImgs, ...toppingsImgs, ...epicesImgs];
+
+    /* 
+    const formData = new FormData(event.target);
+    const bouillon = formData.get('bouillon');
+    const nouilles = formData.get('nouilles');
+    const proteine = formData.get('proteine');
+    const legumes = formData.get('legumes');
+    const toppings = formData.getAll('toppings');
+    const epices = formData.get('epices');
+    */
+  
+    const ramenRecipe = new RamenRecipe(bouillon, nouilles, proteine, legumes, toppings, epices, ingredientsImgs);
+    RamenRecipe.save(ramenRecipe);
+  });
