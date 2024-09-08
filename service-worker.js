fc7476425771c7d42e5aaee3255283f8a18d951d
Jeanne Pietropaoli <j.pietropaoli@yahoo.fr>
2024-09-08 19:23:08 -0400

    Ajout des fichiers de fonts a la cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "static-cache-v12";
+const CACHE_NAME = "static-cache-v13";
 
 //Add list of files to cache here.
 const FILES_TO_CACHE = [
@@ -12,7 +12,8 @@ const FILES_TO_CACHE = [
   "fontawesome-free-6.6.0-web/webfonts/fa-solid-900.woff2",
   "fontawesome-free-6.6.0-web/webfonts/fa-regular-400.woff2",
   "fontawesome-free-6.6.0-web/webfonts/fa-brands-400.woff2",
-  "https://fonts.googleapis.com/css2?family=Oleo+Script:wght@400;700&display=swap",
+  "fonts/oleo/OleoScript-Bold.ttf",
+  "fonts/oleo/OleoScript-Regular.ttf",
   "scripts/collapse.js",
   "scripts/mobile_menu.js",
   "scripts/paralax.js",
