80f17ed727796b26bba01b857f4b8dde09591522
Jeanne Pietropaoli <j.pietropaoli@yahoo.fr>
2024-09-09 11:23:44 -0400

    Ajout des images a la cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "static-cache-v35";
+const CACHE_NAME = "static-cache-v36";
 
 // Fichiers a stocker dans la cache
 const FILES_TO_CACHE = [
@@ -22,13 +22,80 @@ const FILES_TO_CACHE = [
   "scripts/recipe-display.js"
 ];
 
+const IMAGES_TO_CACHE = [
+  "img/favicon.png",
+  "img/ingredeints-bg.png",
+  "img/ingredeints-bg-checked.png",
+  "img/ramen_bol.png",
+  "img/ramen-semaine.png",
+  "img/whatsoup-blue.png",
+  "img/etapes/bouillon_miso_avec_bg.png",
+  "img/etapes/bouillon_shio_avec_bg.png",
+  "img/etapes/bouillon_shoyu_avec_bg.png",
+  "img/etapes/bouillon_tonkotsu_avec_bg.png",
+  "img/etapes/bouillon-photo-2.jpg",
+  "img/etapes/bouillon-photo.jpg",
+  "img/etapes/casserole.png",
+  "img/etapes/nouilles.png",
+  "img/etapes/passoire.png",
+  "img/etapes/toppings-photo.jpg",
+  "img/ingredients/bouillon/bol-v1-trs.png",
+  "img/ingredients/bouillon/bol-v2-trspng.png",
+  "img/ingredients/bouillon/bol-v3-trs.png",
+  "img/ingredients/bouillon/bol-v4-trs.png",
+  "img/epices/ail_poudre.png",
+  "img/epices/gingembre_poudre.png",
+  "img/epices/huile_sesame.png",
+  "img/epices/kimchi.png",
+  "img/epices/piment_poudre.png",
+  "img/epices/poivre_noir.png",
+  "img/epices/shichimi_togarashi.png",
+  "img/epices/yuzu_kosho.png",
+  "img/legumes/bambou.png",
+  "img/legumes/basilic.png",
+  "img/legumes/brocoli.png",
+  "img/legumes/champignons.png",
+  "img/legumes/chou.png",
+  "img/legumes/epinards.png",
+  "img/legumes/fenouil.png",
+  "img/legumes/feuille-chou.png",
+  "img/legumes/mais.png",
+  "img/legumes/oignons_verts.png",
+  "img/legumes/poireau.png",
+  "img/legumes/pousses_soja.png",
+  "img/legumes/radis.png",
+  "img/legumes/shiitake.png",
+  "img/nouilles/plates-trs.png",
+  "img/nouilles/thick-trs.png",
+  "img/nouilles/thin-trs.png",
+  "img/nouilles/wavy-trs.png",
+  "img/proteines/boeuf.png",
+  "img/proteines/crevette.png",
+  "img/proteines/porc.png",
+  "img/proteines/poulet.png",
+  "img/proteines/tempura_crevette.png",
+  "img/proteines/tofu.png",
+  "img/toppings/ail_frit.png",
+  "img/toppings/algues.png",
+  "img/toppings/beurre.png",
+  "img/toppings/dumplings.png",
+  "img/toppings/gingembre.png",
+  "img/toppings/narutomaki.png",
+  "img/toppings/oeuf-mollet.png",
+  "img/toppings/oeuf.png",
+  "img/toppings/sesame.png",
+  "img/presntation_cards/creer.jpg",
+  "img/presntation_cards/s_inspirer.jpg",
+  "img/presntation_cards/tester.jpg",
+];
+
 // Installation - fait seulement une fois, premiere ouverture du site (installation du service worker)
 self.addEventListener("install", (evt) => {
   // Precache static resources here.
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       console.log("[ServiceWorker] Pre-caching offline page");
-      return cache.addAll(FILES_TO_CACHE);
+      return cache.addAll([...FILES_TO_CACHE, ...IMAGES_TO_CACHE]);
     })
   );
   self.skipWaiting();
@@ -58,10 +125,8 @@ self.addEventListener("activate", (evt) => {
 // Fetch event - Acces aux ressources
 self.addEventListener("fetch", (evt) => {
   console.log("[ServiceWorker] Fetch", evt.request.url);
-
+/*
   // Permet de mettre en cache dynamiquement les images avec une strategie stale-while-revalidate, c'est a dire que l'image sera affichee meme si elle est obsolete, et sera remplacee par la nouvelle image une fois telechargee
-  
-  // faire en sorte de cacher les images qui ne se trouvent pad sur la page sur laquelle on est sinon hors ligne ne seront pas cachees deja
   if (evt.request.url.includes('/img/')) {
     evt.respondWith(
       caches.match(evt.request).then((cachedResponse) => {
@@ -79,6 +144,7 @@ self.addEventListener("fetch", (evt) => {
     );
     return;
   }
+    */
 
   if (evt.request.mode !== 'navigate') {
     // Not a page navigation, bail.
